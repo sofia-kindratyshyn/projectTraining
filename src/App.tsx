72458f7ca3def6103668f6587e2eca4125506cfc
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-08-19 14:29:28 +0300

    Added buttons, cards and bages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import Buttons from "./components/Buttons";
-import Card from "./components/Card";
+import CardList from "./components/CardList";
 
 function App() {
   return (
     <>
       <Buttons />
-      <h2 className="text-4xl text-center mb-6">Cards</h2>
-      <ul className="flex flex-wrap gap-3 justify-center">
-        <li>
-          <Card />
-        </li>
-        <li>
-          <Card />
-        </li>
-      </ul>
+      <CardList />
     </>
   );
 }
